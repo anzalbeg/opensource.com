bd671ce2ea9cd14ef2ab28e7559559c43c0712a1
anzal beg <abeg@firstam.com>
2018-07-18 00:16:33 +0530

    commented compoer

--- HLF/network-artifacts-gen.sh
+++ HLF/network-artifacts-gen.sh
@@ -15,15 +15,15 @@ ORG1_DOMAIN="org1.example.com"
 ORG2_DOMAIN="org2.example.com"
 
 if [ ! "$(docker images | grep hyperledger/fabric )" ]; then
- docker pull hyperledger/fabric-peer:$FABRIC
- docker pull hyperledger/fabric-ca:$FABRIC
- docker pull hyperledger/fabric-ccenv:$FABRIC
- docker pull hyperledger/fabric-orderer:$FABRIC
- docker pull hyperledger/fabric-couchdb:latest
+ #docker pull hyperledger/fabric-peer:$FABRIC
+ #docker pull hyperledger/fabric-ca:$FABRIC
+ #docker pull hyperledger/fabric-ccenv:$FABRIC
+ #docker pull hyperledger/fabric-orderer:$FABRIC
+ #docker pull hyperledger/fabric-couchdb:latest
 fi
 
 if [ ! -d "bin"]; then
-    curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${BIN}/hyperledger-fabric-${BIN}.tar.gz | tar xz
+    #curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${BIN}/hyperledger-fabric-${BIN}.tar.gz | tar xz
 fi
 
 sleep $SLEEP_TIMEOUT
@@ -32,21 +32,21 @@ sleep $SLEEP_TIMEOUT
 
 sleep $SLEEP_TIMEOUT
 
-KEYSTORE1=`ls ./crypto-config/peerOrganizations/$ORG1_DOMAIN/users/Admin@$ORG1_DOMAIN/msp/keystore`
-composer identity import \
--p hlfv1 \
--u Admin@$ORG1_DOMAIN \
--c ./crypto-config/peerOrganizations/$ORG1_DOMAIN/users/Admin@$ORG1_DOMAIN/msp/signcerts/Admin@$ORG1_DOMAIN-cert.pem \
--k ./crypto-config/peerOrganizations/$ORG1_DOMAIN/users/Admin@$ORG1_DOMAIN/msp/keystore/$KEYSTORE1
+# KEYSTORE1=`ls ./crypto-config/peerOrganizations/$ORG1_DOMAIN/users/Admin@$ORG1_DOMAIN/msp/keystore`
+# composer identity import \
+# -p hlfv1 \
+# -u Admin@$ORG1_DOMAIN \
+# -c ./crypto-config/peerOrganizations/$ORG1_DOMAIN/users/Admin@$ORG1_DOMAIN/msp/signcerts/Admin@$ORG1_DOMAIN-cert.pem \
+# -k ./crypto-config/peerOrganizations/$ORG1_DOMAIN/users/Admin@$ORG1_DOMAIN/msp/keystore/$KEYSTORE1
 
-sleep $SLEEP_TIMEOUT
+# sleep $SLEEP_TIMEOUT
 
-KEYSTORE2=`ls ./crypto-config/peerOrganizations/$ORG2_DOMAIN/users/Admin@$ORG2_DOMAIN/msp/keystore`
-composer identity import \
--p hlfv1 \
--u Admin@$ORG2_DOMAIN \
--c ./crypto-config/peerOrganizations/$ORG2_DOMAIN/users/Admin@$ORG2_DOMAIN/msp/signcerts/Admin@$ORG2_DOMAIN-cert.pem \
--k ./crypto-config/peerOrganizations/$ORG2_DOMAIN/users/Admin@$ORG2_DOMAIN/msp/keystore/$KEYSTORE2
+# KEYSTORE2=`ls ./crypto-config/peerOrganizations/$ORG2_DOMAIN/users/Admin@$ORG2_DOMAIN/msp/keystore`
+# composer identity import \
+# -p hlfv1 \
+# -u Admin@$ORG2_DOMAIN \
+# -c ./crypto-config/peerOrganizations/$ORG2_DOMAIN/users/Admin@$ORG2_DOMAIN/msp/signcerts/Admin@$ORG2_DOMAIN-cert.pem \
+# -k ./crypto-config/peerOrganizations/$ORG2_DOMAIN/users/Admin@$ORG2_DOMAIN/msp/keystore/$KEYSTORE2
 
 sleep $SLEEP_TIMEOUT
 
