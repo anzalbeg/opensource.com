bd34fb3075ae7038dea9ba1d51cd6d3a0e4a5d9b
anzal beg <abeg@firstam.com>
2018-07-19 00:36:38 +0530

    added org specific namespace

--- HLF/channel.sh
+++ HLF/channel.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export GENESIS_BLOCK=/etc/crypto-config/opensource.com/HLF/channel-artifacts/orderer-channel.tx
-export POD_ID=`kubectl get pod | grep peer0 | cut -f1 -d' '`
+export POD_ID=`kubectl get pod | grep peer1 | cut -f1 -d' '`
 export ORDERER_ADDR="192.168.99.100"
 export ORG_DOMAIN="org1.example.com"
 
@@ -8,25 +8,9 @@ kubectl exec $POD_ID -it --  bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config
 
 kubectl exec $POD_ID -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=0.0.0.0:7051 && peer channel join -b mychannel.block -o $ORDERER_ADDR:7050"
 
-export GENESIS_BLOCK=/etc/crypto-config/opensource.com/HLF/channel-artifacts/orderer-channel.tx
-export POD_ID=`kubectl get pod | grep peer1 | cut -f1 -d' '`
-export ORDERER_ADDR="192.168.99.100"
-export ORG_DOMAIN="org1.example.com"
-
-
-kubectl exec $POD_ID -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=0.0.0.0:7051 && peer channel join -b mychannel.block -o $ORDERER_ADDR:7050"
-
 
 export GENESIS_BLOCK=/etc/crypto-config/opensource.com/HLF/channel-artifacts/orderer-channel.tx
 export POD_ID=`kubectl get pod | grep peer2 | cut -f1 -d' '`
-export ORDERER_ADDR="192.168.99.100"
-export ORG_DOMAIN="org2.example.com"
-
-
-kubectl exec $POD_ID -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=0.0.0.0:7051 && peer channel join -b mychannel.block -o $ORDERER_ADDR:7050"
-
-export GENESIS_BLOCK=/etc/crypto-config/opensource.com/HLF/channel-artifacts/orderer-channel.tx
-export POD_ID=`kubectl get pod | grep peer3 | cut -f1 -d' '`
 export ORDERER_ADDR="192.168.99.100"
 export ORG_DOMAIN="org2.example.com"
 
