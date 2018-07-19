4a889dde9f6453d075b4c37db40f85cf6801ffb6
anzal beg <abeg@firstam.com>
2018-07-19 17:41:26 +0530

    add cli container

--- HLF/channel.sh
+++ HLF/channel.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 export GENESIS_BLOCK=/etc/crypto-config/opensource.com/HLF/channel-artifacts/orderer-channel.tx
-export POD_ID=`kubectl get pod --namespace org1namespace | grep peer1 | cut -f1 -d' '`
-export ORDERER_ADDR="orderer"
+export POD_ID=`kubectl get pod --namespace org1namespace | grep cli | cut -f1 -d' '`
+export ORDERER_ADDR="10.60.0.61"
 export ORG_DOMAIN="org1.example.com"
+export CHAINCODE_PATH="/etc/crypto-config/opensource.com/HLF/chaincode/chaincode_example02"
 
-#kubectl exec $POD_ID --namespace org1namespace -it --  bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=0.0.0.0:7051 peer channel create -o $ORDERER_ADDR:7050 -c mychannel -f $GENESIS_BLOCK"
+#creating channel name mychannel
+kubectl exec $POD_ID --namespace org1namespace -it --  bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=peer1.org1.example.com:7051 peer channel create -o $ORDERER_ADDR:7050 -c mychannel -f $GENESIS_BLOCK"
 
-#kubectl exec $POD_ID --namespace org1namespace -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=0.0.0.0:7051 && peer channel join -b mychannel.block -o $ORDERER_ADDR:7050"
+#joining mychannel with peer1
+kubectl exec $POD_ID --namespace org1namespace -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=peer1.org1.example.com:7051 && peer channel join -b mychannel.block -o $ORDERER_ADDR:7050"
 
-kubectl exec $POD_ID --namespace org1namespace -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=0.0.0.0:7051 && peer chaincode install -n mycc -v 1.0 -p github.com/hyperledger/SmartProperty/chaincode/smartProperty"
+#installing chaincode on peer1
+kubectl exec $POD_ID --namespace org1namespace -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=peer1.org1.example.com:7051 && peer chaincode install -n mycc -v 1.0 -p $CHAINCODE_PATH"
 
+#setting up the peer2 env variables
+export GENESIS_BLOCK=/etc/crypto-config/opensource.com/HLF/channel-artifacts/orderer-channel.tx
+export POD_ID=`kubectl get pod --namespace org2namespace | grep cli | cut -f1 -d' '`
+export ORDERER_ADDR="10.60.0.61"
+export ORG_DOMAIN="org2.example.com"
+export CHAINCODE_PATH="/etc/crypto-config/opensource.com/HLF/chaincode/chaincode_example02"
 
-#export GENESIS_BLOCK=/etc/crypto-config/opensource.com/HLF/channel-artifacts/orderer-channel.tx
-#export POD_ID=`kubectl get pod --namespace org2namespace | grep peer2 | cut -f1 -d' '`
-#export ORDERER_ADDR="orderer"
-#export ORG_DOMAIN="org2.example.com"
+#joining mychannel with peer2
+kubectl exec $POD_ID --namespace org1namespace -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=peer2.org2.example.com:7051 && peer channel join -b mychannel.block -o $ORDERER_ADDR:7050"
 
-
-#kubectl exec $POD_ID --namespace org2namespace -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=0.0.0.0:7051 && peer channel join -b mychannel.block -o $ORDERER_ADDR:7050"
+#installing chaincode on peer2
+kubectl exec $POD_ID --namespace org1namespace -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=peer2.org2.example.com:7051 && peer chaincode install -n mycc -v 1.0 -p $CHAINCODE_PATH"
