63362ddf31d0b76e9110981f4e2ec8e6a0e29f3a
anzal beg <abeg@firstam.com>
2018-07-21 02:57:58 +0530

    added

--- HLF/instantiate.sh
+++ HLF/instantiate.sh
@@ -24,8 +24,8 @@
 
 export GENESIS_BLOCK=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/channel-artifacts/orderer-channel.tx
 export CLI_POD_ID=`kubectl get pod --namespace org1namespace | grep cli | cut -f1 -d' '`
-export ORDERER_POD_ID=`kubectl get pod --namespace orderernamepsace | grep orderer | cut -f1 -d' '`
-export ORDERER_ADDR=`kubectl get --all-namespaces  --output json  pods | jq '.items[] | select(.metadata.name=="'$ORDERER_POD_ID'")' | jq .status.podIP`
+export ORDERER_POD_ID=`kubectl get pod | grep orderer | cut -f1 -d' '`
+export ORDERER_ADDR=`kubectl get --output json  pods | jq '.items[] | select(.metadata.name=="'$ORDERER_POD_ID'")' | jq .status.podIP`
 export ORG_DOMAIN="org1.example.com"
 export CHAINCODE_PATH=github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/chaincode/chaincode_example02/go
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp
@@ -37,7 +37,7 @@ export CHANNEL_NAME="mychannel"
 export CORE_CHAINCODE_EXECUTETIMEOUT=150000
 
 #create channel
-kubectl exec $CLI_POD_ID --namespace org1namespace -it --  bash -c "CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH && CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS peer channel create --logging-level=DEBUG -o $ORDERER_ADDR:7050 -c mychannel -f $GENESIS_BLOCK"
+#kubectl exec $CLI_POD_ID --namespace org1namespace -it --  bash -c "CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH && CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS peer channel create --logging-level=DEBUG -o $ORDERER_ADDR:7050 -c mychannel -f $GENESIS_BLOCK"
 
 # # fetch first block from orderer
 
@@ -55,7 +55,7 @@ kubectl exec $CLI_POD_ID --namespace org1namespace -it --  bash -c "CORE_PEER_MS
 
 #instantiating chaincode
 
- #kubectl exec $CLI_POD_ID --namespace org1namespace -it -- bash -c "CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID && CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH && CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS && CORE_CHAINCODE_EXECUTETIMEOUT=$CORE_CHAINCODE_EXECUTETIMEOUT && peer chaincode instantiate -o $ORDERER_ADDR:7050 -C $CHANNEL_NAME -n mycc -v 1.0 -c '{\"Args\":[\"init\",\"a\", \"100\", \"b\",\"200\"]}' -P \"OR ('Org1MSP.member','Org2MSP.member')\""
+ kubectl exec $CLI_POD_ID --namespace org1namespace -it -- bash -c "CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID && CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH && CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS && peer chaincode instantiate -o $ORDERER_ADDR:7050 -C $CHANNEL_NAME -n mycc -v 1.0 -c '{\"Args\":[\"init\",\"a\", \"100\", \"b\",\"200\"]}' -P \"OR ('Org1MSP.member','Org2MSP.member')\""
 
 
 
--- HLF/network-artifacts-gen.sh
+++ HLF/network-artifacts-gen.sh
@@ -1,36 +1,37 @@
 #!/bin/sh
 
-set -ev
 
-export VERSION=1.1.0
+
+export VERSION=1.0.5
 export ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
 
 MARCH=`uname -m`
 
 FABRIC="$MARCH-$VERSION"
 BIN="$ARCH-$VERSION"
-SLEEP_TIMEOUT=10
+SLEEP_TIMEOUT=0
 
 ORG1_DOMAIN="org1.example.com"
 ORG2_DOMAIN="org2.example.com"
 
-if [ ! "$(docker images | grep hyperledger/fabric )" ]; then
- docker pull hyperledger/fabric-peer:$FABRIC
- docker pull hyperledger/fabric-ca:$FABRIC
- docker pull hyperledger/fabric-ccenv:$FABRIC
- docker pull hyperledger/fabric-orderer:$FABRIC
- docker pull hyperledger/fabric-couchdb:latest
-fi
+# if [ ! "$(docker images | grep hyperledger/fabric )" ]; then
+#  docker pull hyperledger/fabric-peer:$FABRIC
+#  docker pull hyperledger/fabric-ca:$FABRIC
+#  docker pull hyperledger/fabric-ccenv:$FABRIC
+#  docker pull hyperledger/fabric-orderer:$FABRIC
+#  docker pull hyperledger/fabric-couchdb:latest
+# fi
 
 #if [ ! -d "bin"]; then
-#    curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${BIN}/hyperledger-fabric-${BIN}.tar.gz | tar xz
+ echo "heelo bin"
+   curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${BIN}/hyperledger-fabric-${BIN}.tar.gz | tar xz
 #fi
 
-sleep $SLEEP_TIMEOUT
+# sleep $SLEEP_TIMEOUT
 
-./bin/cryptogen generate --config=./crypto-config.yaml
+# ./bin/cryptogen generate --config=./crypto-config.yaml
 
-sleep $SLEEP_TIMEOUT
+# sleep $SLEEP_TIMEOUT
 
 # KEYSTORE1=`ls ./crypto-config/peerOrganizations/$ORG1_DOMAIN/users/Admin@$ORG1_DOMAIN/msp/keystore`
 # composer identity import \
@@ -56,8 +57,10 @@ OUTPUT_DIR=$PWD/channel-artifacts
 
 mkdir -p $OUTPUT_DIR
 
-./bin/configtxgen -profile TwoOrgsOrdererGenesis -outputBlock $OUTPUT_DIR/orderer-genesis.block
-./bin/configtxgen -profile TwoOrgsChannel -outputCreateChannelTx $OUTPUT_DIR/orderer-channel.tx -channelID mychannel
+#./bin/configtxgen -profile TwoOrgsOrdererGenesis -outputBlock $OUTPUT_DIR/orderer-genesis.block
+#./bin/configtxgen -profile TwoOrgsChannel -outputCreateChannelTx $OUTPUT_DIR/orderer-channel.tx -channelID mychannel
+./bin/configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate $OUTPUT_DIR/Org1MSPanchors.tx -channelID mychannel -asOrg Org1MSP
+./bin/configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate $OUTPUT_DIR/Org2MSPanchors.tx -channelID mychannel -asOrg Org2MSP
 
 echo "... 🙌🏿"
   
