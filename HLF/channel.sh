bc584044dc2da07f8e5c200c21aa8a86b43f7145
anzal beg <abeg@firstam.com>
2018-07-22 15:13:31 +0530

    error removed

--- HLF/channel.sh
+++ HLF/channel.sh
@@ -20,4 +20,5 @@ export CORE_CHAINCODE_EXECUTETIMEOUT="150000"
 
 # create channel
 
-kubectl exec $CLI_POD_ID --namespace org1namespace -it --  bash -c "CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH && CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS peer channel create --logging-level=DEBUG -o $ORDERER_ADDR -c mychannel -f $GENESIS_BLOCK"
+#kubectl exec $CLI_POD_ID --namespace org1namespace -it --  bash -c "CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH && CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS peer channel create --logging-level=DEBUG -o $ORDERER_ADDR -c mychannel -f $GENESIS_BLOCK"
+kubectl exec $CLI_POD_ID --namespace org1namespace -it --  bash -c "peer channel create --logging-level=DEBUG -o $ORDERER_ADDR -c mychannel -f $GENESIS_BLOCK"
