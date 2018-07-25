5ec3be22917351ce7586eafb8f8a11850d6c3074
anzal beg <abeg@firstam.com>
2018-07-25 11:04:54 +0530

    modified configtx.yaml

--- HLF/joinChannel.sh
+++ HLF/joinChannel.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# setting up the peer1 env variables inside cli environment
+# setting up the peer0org1 env variables inside cli environment
 
 export GENESIS_BLOCK=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/channel-artifacts/orderer-channel.tx
-export CLI_POD_ID=`kubectl get pod --namespace org1namespace | grep cli | cut -f1 -d' '`
+export CLI_POD_ID=`kubectl get pod | grep cli | cut -f1 -d' '`
 #export ORDERER_POD_ID=`kubectl get pod | grep orderer | cut -f1 -d' '`
 #export ORDERER_ADDR=`kubectl get --output json  pods | jq '.items[] | select(.metadata.name=="'$ORDERER_POD_ID'")' | jq .status.podIP`
-export ORDERER_ADDR="35.184.113.51:30724"
+export ORDERER_ADDR="10.63.248.14:7050"
 export ORG_DOMAIN="org1.example.com"
 export CHAINCODE_PATH=github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/chaincode/chaincode_example02/go
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp
@@ -14,20 +14,43 @@ export CORE_PEER_LOCALMSPID="Org1MSP"
 #export PEER_POD_ID=`kubectl get pod --namespace org1namespace | grep peer1 | cut -f1 -d' '`
 #export PEER_ADDRESS=`kubectl get --all-namespaces  --output json  pods | jq '.items[] | select(.metadata.name=="'$PEER_POD_ID'")' | jq .status.podIP`
 #export CORE_PEER_ADDRESS="$PEER_ADDRESS:7051"
-export CORE_PEER_ADDRESS="35.193.91.66:32009"
+export CORE_PEER_ADDRESS="10.63.240.194:7051"
 export CHANNEL_NAME="mychannel"
 
 # sleep 10
 
-# # # joining mychannel with peer1
- kubectl exec $CLI_POD_ID --namespace org1namespace -it -- bash -c "CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID && CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH && CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS && peer channel join -b mychannel.block -o $ORDERER_ADDR"
+# joining mychannel with peer1
+ kubectl exec $CLI_POD_ID -it -- bash -c "CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID && CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH && CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS && peer channel join -b mychannel.block -o $ORDERER_ADDR"
+
+
+# join peer1org1
+export GENESIS_BLOCK=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/channel-artifacts/orderer-channel.tx
+export CLI_POD_ID=`kubectl get pod | grep cli | cut -f1 -d' '`
+#export ORDERER_POD_ID=`kubectl get pod | grep orderer | cut -f1 -d' '`
+#export ORDERER_ADDR=`kubectl get --output json  pods | jq '.items[] | select(.metadata.name=="'$ORDERER_POD_ID'")' | jq .status.podIP`
+export ORDERER_ADDR="10.63.248.14:7050"
+export ORG_DOMAIN="org1.example.com"
+export CHAINCODE_PATH=github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/chaincode/chaincode_example02/go
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp
+export CORE_PEER_LOCALMSPID="Org1MSP"
+#export PEER_POD_ID=`kubectl get pod --namespace org1namespace | grep peer1 | cut -f1 -d' '`
+#export PEER_ADDRESS=`kubectl get --all-namespaces  --output json  pods | jq '.items[] | select(.metadata.name=="'$PEER_POD_ID'")' | jq .status.podIP`
+#export CORE_PEER_ADDRESS="$PEER_ADDRESS:7051"
+export CORE_PEER_ADDRESS="10.63.241.28:7051"
+export CHANNEL_NAME="mychannel"
+
+# sleep 10
+
+# # # joining mychannel with peer0org2
+ kubectl exec $CLI_POD_ID -it -- bash -c "CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID && CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH && CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS && peer channel join -b mychannel.block -o $ORDERER_ADDR"
+
 
 #setting up the peer2 env variables inside cli environment
 export GENESIS_BLOCK=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/channel-artifacts/orderer-channel.tx
-export CLI_POD_ID=`kubectl get pod --namespace org1namespace | grep cli | cut -f1 -d' '`
+export CLI_POD_ID=`kubectl get pod | grep cli | cut -f1 -d' '`
 #export ORDERER_POD_ID=`kubectl get pod | grep orderer | cut -f1 -d' '`
 #export ORDERER_ADDR=`kubectl get --output json  pods | jq '.items[] | select(.metadata.name=="'$ORDERER_POD_ID'")' | jq .status.podIP`
-export ORDERER_ADDR="35.184.113.51:30724"
+export ORDERER_ADDR="10.63.248.14:7050"
 export ORG_DOMAIN="org2.example.com"
 export CHAINCODE_PATH=github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/chaincode/chaincode_example02/go
 export CORE_PEER_MSPCONFIGPATH="/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/opensource.com/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp"
@@ -35,9 +58,9 @@ export CORE_PEER_LOCALMSPID="Org2MSP"
 #export PEER_POD_ID=`kubectl get pod --namespace org2namespace | grep peer2 | cut -f1 -d' '`
 #export PEER_ADDRESS=`kubectl get --all-namespaces  --output json  pods | jq '.items[] | select(.metadata.name=="'$PEER_POD_ID'")' | jq .status.podIP`
 #export CORE_PEER_ADDRESS="$PEER_ADDRESS:7051"
-export CORE_PEER_ADDRESS="104.197.198.61:31771"
+export CORE_PEER_ADDRESS="10.63.246.218:7051"
 export CHANNEL_NAME="mychannel"
 
  # joining mychannel with peer2
 
-  kubectl exec $CLI_POD_ID --namespace org1namespace -it -- bash -c "CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID && CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH && CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS && peer channel join -b mychannel.block -o $ORDERER_ADDR"
+  kubectl exec $CLI_POD_ID -it -- bash -c "CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID && CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH && CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS && peer channel join -b mychannel.block -o $ORDERER_ADDR"
